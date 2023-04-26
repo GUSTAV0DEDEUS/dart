a060e07616d6df87f9f0b2b797e0443b025f39f4
GUSTAV0DEDEUS <112625144+TCC3ADS@users.noreply.github.com>
2023-04-26 10:13:36 -0300

    developing

---
+++ Fundamentos/constante_2.dart
@@ -0,0 +1,8 @@
+main() {
+  var lista = const ['Ana', 'Lia', 'Gui'];
+  // lista = const ['Banana', 'Maça'];
+  // const lista = ['Ana', 'Lia', 'Gui'];
+  // final lista = const ['Ana', 'Lia', 'Gui'];;
+  lista.add('Rebeca');
+  print(lista);
+}
